5f658674c5b663a7bd47d14a365a37c8d6237f43
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2020-12-03 18:42:35 +0530

    Add express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+const express = require('express')
+
+const app = express()
+
+app.use((req, res) => {
+  res.status(200).send({
+    status: `ok`,
+  })
+})
+
+app.listen(process.env.PORT)
